f152efd2eb0651e65078e57ec9e56d37fe66e99c
s22575521-arch <s22575521@gmail.com>
2026-07-28 21:20:40 +0300

    Create calculator.py

---
+++ Python/calculator.py
@@ -0,0 +1,12 @@
+print("| Welcome to Calculator App |")
+print()
+
+number1 = int(input("First Number: "))
+number2 = int(input("Second Number: "))
+
+print("|----------------------------------|")
+print("Adding:", number1 + number2)
+print("Subtract:", number1 - number2)
+print("Multiply:", number1 * number2)
+print("Divide:", number1 / number2)
+print("Modulus:", number1 % number2)
