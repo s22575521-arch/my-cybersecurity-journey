97bb212344d1ed0e300cdfa543fd27c06227287b
SARA AL-GHASSAN <s22575521@gmail.com>
2026-09-06 14:55:49 +0300

    Create invitation_check.py

---
+++ Python/invitation_check.py
@@ -0,0 +1,9 @@
+has_invitation = input("Do u have an invitation ?(yes/no)")
+is_formal = input("Are wearing fromal ?(yes/no)")
+if has_invitation == "yes" and  is_formal== "yes":
+    print("Welcom")
+elif has_invitation == "yes" or  is_formal== "yes":
+    print("OK")
+else:
+    print("sorry")
+#python sara.py
